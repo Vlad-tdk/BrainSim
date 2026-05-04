d3bfcc7a72b0d47c1346ca6367757189f795eea4
Vlad <xalif80@gmail.com>
2026-05-04 16:15:47 +0200

    Initial Commit

---
+++ BrainSim/BrainSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainSimApp.swift
+//  BrainSim
+//
+//  Created by Vladimir Martemianov on 4. 5. 2026..
+//
+
+import SwiftUI
+
+@main
+struct BrainSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
